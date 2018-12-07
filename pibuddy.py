20dba8fad289c28b18cbdc966a326946fdf3495a
Matty Gallantry <matt@wakelet.com>
2018-12-07 12:57:45 +0000

    added change

--- pibuddy.py
+++ pibuddy.py
@@ -4,7 +4,9 @@
 # This file originates from Bluups PiBuddy Project
 # Author: Bluup
 
+import psutil
 from retroarchapi import RetroArchPythonApi
 
 api = RetroArchPythonApi(retroarch_path='/opt/retropie/emulators/retroarch/bin/retroarch',settings_path='settings')
-api.toggle_pause()
+[p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'python' in p.info['retroarch']]
+# api.toggle_pause()
