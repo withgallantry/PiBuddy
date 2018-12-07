5c5a47e9f02017a299f8e6f91e49384057a34990
Matty Gallantry <matt@wakelet.com>
2018-12-07 16:05:55 +0000

    Test

--- net_pibuddy.py
+++ net_pibuddy.py
@@ -7,7 +7,7 @@ import os
 import subprocess
 
 try:
-    subprocess.call("echo -n \"RESET\" | nc -u -w1 127.0.0.1 55355")
+    subprocess.call('echo -n "RESET" | nc -u -w1 127.0.0.1 55355')
     print "Executed Reset"
 except:
     print "Cannot execute"
