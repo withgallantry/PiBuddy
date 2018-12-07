b867ff30d400355a513f660ffbfe3ed28903320b
Matty Gallantry <matt@wakelet.com>
2018-12-07 13:00:38 +0000

    Added new retroarch

--- pibuddy.py
+++ pibuddy.py
@@ -3,10 +3,15 @@
 #
 # This file originates from Bluups PiBuddy Project
 # Author: Bluup
-
-import psutil
+import os
 from retroarchapi import RetroArchPythonApi
 
 api = RetroArchPythonApi(retroarch_path='/opt/retropie/emulators/retroarch/bin/retroarch',settings_path='settings')
-[p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'python' in p.info['retroarch']]
+
+
+def getpid(process_name):
+    return [item.split()[1] for item in os.popen('tasklist').read().splitlines()[4:] if process_name in item.split()]
+
+# [p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'python' in p.info['retroarch']]
 # api.toggle_pause()
+print(getpid('retroarch'))
