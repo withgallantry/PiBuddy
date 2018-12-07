8c6959b31493e2aa414d89bc21cd3cf79bea51b4
Matty Gallantry <matt@wakelet.com>
2018-12-07 15:13:58 +0000

    New

--- pibuddy.py
+++ pibuddy.py
@@ -13,7 +13,7 @@ def getRetroarchPid():
 # api.toggle_pause()
 retroarchPid = getRetroarchPid()
 
-_process_stdin = open(os.path.join('/proc', str(retroarchPid), 'fd', '0'), 'a')
+_process_stdin = open(os.path.join('/proc', retroarchPid, 'fd', '0'), 'a')
 print _process_stdin
 
 _process_stdin.write('checkalive\n')
