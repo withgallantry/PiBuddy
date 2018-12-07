56dc322ec2c64a6d5730a8511ae50c5edc783a48
Matty Gallantry <matt@wakelet.com>
2018-12-07 13:03:13 +0000

    test now

--- pibuddy.py
+++ pibuddy.py
@@ -9,9 +9,9 @@ from retroarchapi import RetroArchPythonApi
 api = RetroArchPythonApi(retroarch_path='/opt/retropie/emulators/retroarch/bin/retroarch',settings_path='settings')
 
 
-def getpid(process_name):
-    return [item.split()[1] for item in os.popen('tasklist').read().splitlines()[4:] if process_name in item.split()]
+def getpid():
+    return os.popen('pidof retroarch').read()
 
 # [p.info for p in psutil.process_iter(attrs=['pid', 'name']) if 'python' in p.info['retroarch']]
 # api.toggle_pause()
-print(getpid('retroarch'))
+print(getpid())
