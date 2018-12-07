ebe9e004630206d7ae5f63520f928d162e8e2aad
Matty Gallantry <matt@wakelet.com>
2018-12-07 16:19:05 +0000

    done

--- net_pibuddy.py
+++ net_pibuddy.py
@@ -7,7 +7,8 @@ import os
 import subprocess
 
 try:
-    subprocess.call('echo -n "RESET" | nc -u -w1 127.0.0.1 55355')
+    p1 = subprocess.Popen(["echo", "-n", "RESET"], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["nc", "-u", "-w1", "127.0.0.1", "55355"], stdin=p1.stdout)
     print "Executed Reset"
 except:
     print "Cannot execute"
