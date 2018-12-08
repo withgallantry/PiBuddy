46451175a27f100beccc0b773234a17c7ad5bf77
Matty Gallantry <matt@wakelet.com>
2018-12-08 11:09:59 +0000

    Waiting on data

--- gatt.py
+++ gatt.py
@@ -17,4 +17,5 @@ try:
     device.subscribe("6e400003-b5a3-f393-e0a9-e50e24dcca9e",
                      callback=handle_data)
 finally:
-    adapter.stop()
+    print "Waiting on data"
+    # adapter.stop()
