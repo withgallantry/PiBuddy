010e7e3f229c1b896d145ba4558ed5a76ea5acf0
Matty Gallantry <matt@wakelet.com>
2018-12-08 00:42:01 +0000

    new type

--- testb.py
+++ testb.py
@@ -16,4 +16,4 @@ devices = scanner.scan(10.0)
 for dev in devices:
     print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
     for (adtype, desc, value) in dev.getScanData():
-        print "  %s = %s" % (desc, value)
+        print "  %s = %s | %s" % (desc, value, dev.getValueText(adtype))
