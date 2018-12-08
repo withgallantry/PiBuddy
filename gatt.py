214ef33eb0ee6cf687993b4b11668bf7a0d9aa92
Matty Gallantry <matt@wakelet.com>
2018-12-08 12:37:42 +0000

    New Connectong

--- gatt.py
+++ gatt.py
@@ -1,23 +1,21 @@
 import pygatt
-from binascii import hexlify
+import logging
+logging.basicConfig()
+logging.getLogger('pygatt').setLevel(logging.DEBUG)
 
-adapter = pygatt.GATTToolBackend()
+adapter = pygatt.BGAPIBackend()
 
-def handle_data(handle, value):
-    """
-    handle -- integer, characteristic read handle the data was received on
-    value -- bytearray, the data returned in the notification
-    """
-    print("Received data: %s" % hexlify(value))
+def print_this(h, stuff):
+    print(stuff)
 
+adapter.start()
+devices = adapter.scan()
+for device in devices:
+    if "custom_device" in device["name"].lower():
+        print("found device")
+        break
 
-try:
-    adapter.start()
-    device = adapter.connect('80:7d:3a:c4:4c:16')
-
-    device.subscribe("6e400003-b5a3-f393-e0a9-e50e24dcca9e",
-                     callback=handle_data)
-finally:
-    print "Waiting on data"
-    input("Press ENTER to exit")
-    # adapter.stop()
+device = adapter.connect(device["address"])
+chars = device.discover_characteristics() # I get some type of a warning here. "UUID b'ffe1' is of unknown type"
+# value = device.char_read("00002a05-0000-1000-8000-00805f9b34fb") #this address was found by printing the `chars` array
+device.subscribe('6e400003-b5a3-f393-e0a9-e50e24dcca9e', print_this) #ExpectedResponseTimeout
