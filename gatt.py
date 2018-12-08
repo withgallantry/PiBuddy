6c96c2209cfef8283b187adc3b33d0a671d52475
Matty Gallantry <matt@wakelet.com>
2018-12-08 12:57:57 +0000

    Keep open

--- gatt.py
+++ gatt.py
@@ -19,3 +19,6 @@ device = adapter.connect(device["address"])
 chars = device.discover_characteristics() # I get some type of a warning here. "UUID b'ffe1' is of unknown type"
 # value = device.char_read("00002a05-0000-1000-8000-00805f9b34fb") #this address was found by printing the `chars` array
 device.subscribe('6e400003-b5a3-f393-e0a9-e50e24dcca9e', print_this) #ExpectedResponseTimeout
+
+print "Press Enter to continue ..."
+raw_input()
