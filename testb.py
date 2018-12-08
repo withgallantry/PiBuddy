abdb188d7c401e675e6bd1a51a5d80a8c5ac30f4
Matty Gallantry <matt@wakelet.com>
2018-12-08 00:59:38 +0000

    Test

--- testb.py
+++ testb.py
@@ -17,7 +17,7 @@ class PiBuddy():
         self.device = Peripheral(address)
 
     def getServices(self):
-        return self.device.getCharacteristics()
+        return self.device.getCharacteristics(uuid="6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
 
 
 scanner = Scanner().withDelegate(ScanDelegate())
